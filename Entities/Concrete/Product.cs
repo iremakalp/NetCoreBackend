fbd17dbe89edec1af455c27c10df0564836a67e5
iremakalp1@hotmail.com <iremakalp1@hotmail.com>
2022-01-27 13:26:58 +0300

    Entities Layer added

--- Entities/Concrete/Product.cs
+++ Entities/Concrete/Product.cs
@@ -1,6 +1,7 @@
 ﻿using Core.Entities.Abstract;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace Entities.Concrete
@@ -12,6 +13,8 @@ namespace Entities.Concrete
     public class Product : IEntity
     {
         public int ProductId { get; set; }
+        // [Required] --> SOLID de S'ye aykiri kullanim
+        // entity yonetilen yerde business parcasi kullanildi
         public string ProductName { get; set; }
         public int Stock { get; set; }
         public int CategoryId { get; set; }
