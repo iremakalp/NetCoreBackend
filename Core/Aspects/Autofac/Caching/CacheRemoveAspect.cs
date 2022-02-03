a2c189425694ea8a665e1e9068d4ebcfa708c399
iremakalp1@hotmail.com <iremakalp1@hotmail.com>
2022-02-03 19:04:06 +0300

    Cache remove aspect added

--- Core/Aspects/Autofac/Caching/CacheRemoveAspect.cs
+++ Core/Aspects/Autofac/Caching/CacheRemoveAspect.cs
@@ -16,7 +16,7 @@ namespace Core.Aspects.Autofac.Caching
             _cacheManager = ServiceTool.ServiceProvider.GetService<ICacheManager>();
         }
 
-        // ekleme,silme guncelleme islemlerinde uygulanmali
+        // ekleme,silme guncelleme islemleri basarili oldugunda uygulanmali
         protected override void OnSuccess(IInvocation invocation)
         {
             _cacheManager.RemoveByPattern(_pattern);
