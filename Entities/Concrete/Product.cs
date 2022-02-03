f23112de4dd8a8b479953392ebdb75726fff0e62
iremakalp1@hotmail.com <iremakalp1@hotmail.com>
2022-02-03 19:04:55 +0300

    Entities Layer added

--- Entities/Concrete/Product.cs
+++ Entities/Concrete/Product.cs
@@ -11,7 +11,7 @@ namespace Entities.Concrete
     {
         public int ProductId { get; set; }
         public string ProductName { get; set; }
-        public int Stock { get; set; }
+        public int UnitsInStock { get; set; }
         public int CategoryId { get; set; }
         public double? UnitPrice { get; set; }      
 
